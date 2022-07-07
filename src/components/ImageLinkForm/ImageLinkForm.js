77b5571749cacd276eed0f4e14c1800e1be3fee1
Hakeem Dahou <Hdahou@egain.com>
2022-07-07 16:41:20 +0100

    restoring old source code

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return(
         <div>
             <p className = 'f3' >
-                {'This Smart Brain Application detects faces in images. Give it a try!!'}
+                {'This Smart Brain Application detects faces in images. Give it a try!'}
             </p>
             <div className = 'center'>
                 <div className = 'form center pa4 br3 shadow-5'>
